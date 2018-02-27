7ce28630803de97b218d30c87233f6fe12619b5c
Luc Didry <luc@didry.org>
2018-02-27 21:15:32 +0100

    Small perl style changes

--- TapTempo.pl
+++ TapTempo.pl
@@ -2,15 +2,15 @@
 
 use strict;
 use warnings;
+use 5.10.0;
 use Time::HiRes;
 use Getopt::Long;
 
 Getopt::Long::Configure ("bundling", "ignorecase_always");
-my $debug=0;
 
-my %args = ('help' => 0, 'version' => 0);;
-my ($precision, $sample, $reset, $version, $key, $tempo, $t) = (0, 5, 5, "a01",,,);
-my @times=();
+my $version = "a01";
+my ($debug, $help, $show_version, $precision, $sample, $reset, $key, $tempo, $t) = (0, 0, 0, 0, 5, 5, '', '', '');
+my @times = ();
 #$key : touche appuyé par l'utilisteur
 #@times : contient les "dates" des frappes
 #$tempo : tempo calculé
@@ -21,96 +21,99 @@ my @times=();
 #$t : temps actuel
 
 #Gestion des paramètres
-GetOptions(\%args, 'help|h', 'version|v');
-GetOptions('percision|p=i' => \$precision, 'reset-time|r=i' => \$reset, 'sample-size|s=i' => \$sample);
+GetOptions(
+    'precision|p=i'   => \$precision,
+    'reset-time|r=i'  => \$reset,
+    'sample-size|s=i' => \$sample,
+    'debug|d'         => \$debug,
+    'help|h'          => \$help,
+    'version|v'       => \$show_version
+);
 
-if($args{'help'} or $args{'version'})
+if($help or $show_version)
 {
-    if($args{'help'})
+    if($help)
     {
-	print "\t-h, --help \taffiche ce message d'aide\n";
-	print "\t-p, --precision \tchanger le nombre de décimale du tempo à afficher\n";
-	print "\t\t\tla valeur par défaut est 0 décimales, le max est 5 décimales\n";
-	print "\t-r, --reset-time \tchanger le temps en seconde de remise à zéro du calcul\n";
-	print "\t\t\tla valeur par défaut est 5 secondes\n";
-	print "\t-s, --sample-size \tchanger le nombre d'échantillons nécessaires au calcul du tempo\n";
-	print "\t\t\tla valeur par défaut est 5 échantillons\n";
-	print "\t-v, --version \tafficher la version\n";
+        say <<EOF;
+\t-h, --help \t\taffiche ce message d'aide
+\t-p, --precision \tchanger le nombre de décimale du tempo à afficher
+\t\t\t\tla valeur par défaut est 0 décimales, le max est 5 décimales
+\t-r, --reset-time \tchanger le temps en seconde de remise à zéro du calcul
+\t\t\t\tla valeur par défaut est 5 secondes
+\t-s, --sample-size \tchanger le nombre d'échantillons nécessaires au calcul du tempo
+\t\t\t\tla valeur par défaut est 5 échantillons
+\t-v, --version \t\tafficher la version
+EOF
     }
-    if($args{'version'})
+    if($show_version)
     {
-	print "Version ".$version."\n";
+        say "Version $version";
     }
     exit 0;
 }
 
-print "precision : ".$precision."\n"  if $debug;
-print "sample : ".$sample."\n"  if $debug;
-print "reset : ".$reset."\n" if $debug;
-print "version : ".$version."\n" if $debug;
-print "key : ".$key."\n" if $debug;
-print "tempo : ".$tempo."\n" if $debug;
-print "t : ".$t."\n" if $debug;
-print "times : ".@times."\n" if $debug;
+my $times_length = scalar(@times);
+say <<EOF if $debug;
+precision : $precision
+sample : $sample
+reset : $reset
+version : $version
+key : $key
+tempo : $tempo
+t : $t
+times : $times_length
+EOF
 
 #Initialisation de la lecture de STDIN
-open(TTY, "+</dev/tty") or die "no tty: $!";
-system "stty  cbreak </dev/tty >/dev/tty 2>&1";
-
-print "Appuyer sur la touche entrée en cadence (q pour quitter).\n\n";
-$key = getc(TTY);
+say "Appuyer sur la touche entrée en cadence (Ctrl+d pour quitter).";
+$key = <STDIN>;
 $t = Time::HiRes::gettimeofday();
 push @times, $t;
-print "\n";
 
-if ( $key eq "q")
+if (!defined($key))
 {
     exit 0;
 }
-print "[Appuyer encore sur la touche entrée pour lancer le calcul du tempo...]\n";
-while (1)
+say "[Appuyer encore sur la touche entrée pour lancer le calcul du tempo...]";
+while (defined(my $key = <STDIN>))
 {
-    $key = getc(TTY);
-    print "\n";
+    chomp $key;
     $t = Time::HiRes::gettimeofday();
     push @times, $t;
 
     # Quitter si q est appuyé
-    if ( $key eq "q")
+    if ($key eq "q")
     {
-	exit 0;
+        exit 0;
     }
 
     # Réinitialiser les temps si plus de $reset secondes sont passés
     if (($times[($#times)] - $times[($#times-1)]) > $reset)
     {
-	while (scalar(@times) > 1)
-	{
-	    shift @times;
-	}
+        shift @times while (scalar(@times) > 1);
     }
 
     # Ne conserver que les $sample derniers échantillons
-    while (scalar(@times) > $sample)
-    {
-	shift @times;
-    }
+    shift @times while (scalar(@times) > $sample);
 
-    print "DEBUG current time t : ".$t."\n" if $debug;
-    print "DEBUG times : ".@times."\n" if $debug;
+    if ($debug)
+    {
+        say "DEBUG current time t : $t";
+        say "DEBUG times : ".@times;
+    }
     # Calcul du $tempo
     my $timesMean = 0;
-    for(my $i=0 ; $i < $#times ; $i++)
+    for (my $i=0 ; $i < $#times ; $i++)
     {
-	$timesMean += ($times[$i+1] - $times[$i]);
+        $timesMean += ($times[$i+1] - $times[$i]);
     }
     $timesMean = ($timesMean / scalar(@times));
-    print "DEBUG - timesMean final : ".$timesMean."\n"  if $debug;
+    say "DEBUG - timesMean final : $timesMean" if $debug;
     $tempo = (60/$timesMean);
-    print "DEBUG - tempo : ".$tempo."\n"  if $debug;
+    say "DEBUG - tempo : $tempo" if $debug;
 
     #Affichage du $tempo avec la $precision demandée
-    printf("Tempo : %.${precision}f bpm ", $tempo);
+    printf("Tempo : %.${precision}f bpm", $tempo);
 }
 
 exit 0;
